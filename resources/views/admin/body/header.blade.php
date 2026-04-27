5d189886ce895f3bbf577eb6e1ab560abe061ac7
e.alzoubi <eyadahalzoubi029@gmail.com>
2026-04-27 22:11:21 +0300

    edit name

--- resources/views/admin/body/header.blade.php
+++ resources/views/admin/body/header.blade.php
@@ -97,7 +97,7 @@
                                 <a class="nav-link dropdown-toggle nav-user me-0" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
                                     <img src="{{ asset('backend/assets/images/users/pengu.png')}}" alt="user-image" class="rounded-circle">
                                     <span class="pro-user-name ms-1">
-                                        Mr Pengu <i class="mdi mdi-chevron-down"></i> 
+                                        Mr Pingu <i class="mdi mdi-chevron-down"></i> 
                                     </span>
                                 </a>
                                 <div class="dropdown-menu dropdown-menu-end profile-dropdown ">
