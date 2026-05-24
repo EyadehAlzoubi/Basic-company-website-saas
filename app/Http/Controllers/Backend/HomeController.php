587648ec33ac460f1283a0fdda245743ffc21bae
EyadehAlzoubi <eyadahalzoubi029@gmail.com>
2026-05-24 21:00:13 +0300

    Add HomeController for feature management with methods to list, add, and store features. This includes a notification system for successful feature insertion, enhancing the backend functionality for managing features.

---
+++ app/Http/Controllers/Backend/HomeController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Feature;
+
+class HomeController extends Controller
+{
+    public function AllFeature()
+    {
+        $feature = Feature::latest()->get();
+        return view('admin.backend.feature.all_feature',compact('feature'));
+    }
+
+
+    public function AddFeature()
+    {
+        return view('admin.backend.feature.add_feature');
+    }
+
+
+    public function StoreFeature(Request $request)
+    {
+
+        Feature::create([
+            'title' =>$request->title,
+            'icon' =>$request->icon,      
+            'description' =>$request->description,
+        ]);
+
+        $notification = array(
+            'message'      => 'Feature Inserted Successfully :)',
+            'alert-type'   => 'success'
+        );
+
+        return redirect()->route('all.feature')->with($notification);
+    }
+
+
+
+    
+}
