7599ff0c4c9e0b745af17521dd3a03e7f24dbd20
EyadehAlzoubi <eyadahalzoubi029@gmail.com>
2026-05-28 01:11:00 +0300

    Add methods for retrieving and updating Clarifi entries in HomeController. This includes a new method for displaying a specific Clarifi entry and an update method that handles image uploads, enhancing the backend management of Clarifi data.

--- app/Http/Controllers/Backend/HomeController.php
+++ app/Http/Controllers/Backend/HomeController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Feature;
+use App\Models\Clarifi;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
+
 
 class HomeController extends Controller
 {
@@ -77,6 +81,73 @@ class HomeController extends Controller
             'message'      => 'Feature Deleted Successfully :)',
             'alert-type'   => 'success'
         );
+
+        return redirect()->back()->with($notification);
+    }
+
+
+
+    public function GetClarigies()
+    {
+
+        $clarifi = Clarifi::find(1);
+
+        return view('admin.backend.clarifi.get_clarifi' , compact('clarifi'));
+
+
+    }
+
+
+    public function UpdateClarigies(Request $request)
+    {
+
+        $clar_id = $request->id;
+
+        $clarifi = Clarifi::find($clar_id);
+
+        if($request->file('image'))
+        {
+            $image = $request->file('image');
+            $manager = new ImageManager(new Driver());
+            $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+            $img = $manager->read($image);
+            $img->resize(302,618)->save(public_path('upload/clarifi/'.$name_gen));
+            $save_url = 'upload/clarifi/'.$name_gen;
+
+            if(file_exists(public_path($clarifi->image)))
+            {
+                @unlink(public_path($clarifi->image));
+            }
+
+
+            Clarifi::find($clar_id)->update([
+                'title' =>$request->title,
+                'description' =>$request->description,      
+                'image' =>$save_url,
+            ]);
+    
+            $notification = array(
+                'message'      => 'Clarifi Updated Successfully :)',
+                'alert-type'   => 'success'
+            );
+
+        }else
+        {
+
+            Clarifi::find($clar_id)->update([
+                'title' =>$request->title,
+                'description' =>$request->description,      
+            ]);
+    
+            $notification = array(
+                'message'      => 'Clarifi Updated Successfully :)',
+                'alert-type'   => 'success'
+            );
+
+
+        }
+
+
 
         return redirect()->back()->with($notification);
     }
