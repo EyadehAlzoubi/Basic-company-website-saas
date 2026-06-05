470e6e38c2ab4f8372c95555bdcf0567a32b631c
EyadehAlzoubi <eyadahalzoubi029@gmail.com>
2026-06-05 22:37:55 +0300

    Add FAQs section to sidebar: Introduce a new collapsible menu item for FAQs in the admin panel sidebar, including links to view all FAQs and add new FAQs, enhancing navigation and content management capabilities.

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -145,6 +145,29 @@
                                 </div>
                             </li>
 
+
+                            <li>
+                                <a href="#Faqs" data-bs-toggle="collapse">
+                                    <i data-feather="alert-octagon"></i>
+                                    <span> Faqs Sutup </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <div class="collapse" id="Faqs">
+                                    <ul class="nav-second-level">
+                                        <li>
+                                            <a href="{{ route('all.faqs')}}" class="tp-link">All Faqs</a>
+                                        </li>
+
+
+                                        <li>
+                                            <a href="{{ route('add.connect')}}" class="tp-link">Add Faqs</a>
+                                        </li>
+
+
+                                    </ul>
+                                </div>
+                            </li>
+
                             <li class="menu-title mt-2">General</li>
 
                             <li>
